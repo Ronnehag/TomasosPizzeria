f087b9923fd46a8c71198c57d071f117451120e9
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-02-12 13:24:33 +0100

    added view model to create new dishes.

--- TomasosPizzeria/Controllers/DishController.cs
+++ TomasosPizzeria/Controllers/DishController.cs
@@ -1,23 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TomasosPizzeria.Models.Entities;
 using TomasosPizzeria.Models.ViewModels;
+using TomasosPizzeria.Services;
 
 namespace TomasosPizzeria.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class DishController : Controller
     {
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        [ActionName("EditDish")]
-        [Route("dish/save/{id}")]
-        public IActionResult EditDish(EditDishViewModel mdl)
+        private readonly IDishService _dishService;
+
+        public DishController(IDishService dishService)
         {
-            return null;
+            _dishService = dishService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _dishService.GetDishCategoriesAsync();
+            var ingredients = await _dishService.GetDishIngredientsAsync();
+
+            var viewModel = new NewDishViewModel
+            {
+                SelectedIngredients = new List<int>(),
+                Ingredients = new MultiSelectList(ingredients, "ProduktId", "ProduktNamn"),
+                FoodTypeSelectList = new SelectList(categories, "MatrattTyp1", "Beskrivning")
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(NewDishViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var categories = await _dishService.GetDishCategoriesAsync();
+                var ingredients = await _dishService.GetDishIngredientsAsync();
+                model.Ingredients = new MultiSelectList(ingredients, "ProduktId", "ProduktNamn");
+                model.FoodTypeSelectList = new SelectList(categories, "MatrattTyp1", "Beskrivning");
+                return View(model);
+            }
+
+            return null;
+
+        }
+
     }
 }
---
+++ TomasosPizzeria/Models/ViewModels/NewDishViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TomasosPizzeria.Models.Entities;
+
+namespace TomasosPizzeria.Models.ViewModels
+{
+    public class NewDishViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Ingrediens inte i listan? Ange med fri text för att skapa nya (separera med mellanslag)")]
+        public string IngrediensNotInList { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public int FoodType { get; set; }
+
+        [Required]
+        [Display(Name = "Ingredienser (välj flera, håll ned ctrl)")]
+        public List<int> SelectedIngredients { get; set; }
+
+
+        public MultiSelectList Ingredients { get; set; }
+
+        public SelectList FoodTypeSelectList { get; set; }
+
+    }
+}
