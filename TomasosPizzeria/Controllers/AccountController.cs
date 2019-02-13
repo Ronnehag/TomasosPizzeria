d07fc0b8b3bb01d40fa0e390787f33b8119aa192
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-02-13 15:33:09 +0100

    Finished user validation form.

--- TomasosPizzeria/Controllers/AccountController.cs
+++ TomasosPizzeria/Controllers/AccountController.cs
@@ -13,53 +13,71 @@ namespace TomasosPizzeria.Controllers
     public class AccountController : Controller
     {
         private readonly IUserService _service;
-        private readonly UserManager<AppUser> _usermanager;
+        private readonly UserManager<AppUser> _userManager;
 
         public AccountController(IUserService service, UserManager<AppUser> usermanager)
         {
             _service = service;
-            _usermanager = usermanager;
+            _userManager = usermanager;
         }
 
         [Route("details/{username}")]
         public async Task<IActionResult> EditDetails()
         {
-            var user = await _usermanager.GetUserAsync(User);
+            var user = await _userManager.GetUserAsync(User);
             if (user == null) return Challenge();
 
             var kund = await _service.FindUserAsync(user.Id);
             if (kund != null)
             {
-                var model = new KundViewModel
+                var model = new KundEditDetailsViewModel
                 {
                     Kund = kund,
-                    AnvandarNamn = user.UserName,
+                    UserName = user.UserName,
 
                 };
                 return View(model);
             }
-            return BadRequest(new {Error = "User not found"});
+            return BadRequest(new { Error = "User not found" });
         }
 
         [HttpPost]
         [Route("details/{username}")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditDetails(KundViewModel model) // New kund data
+        public async Task<IActionResult> EditDetails(KundEditDetailsViewModel model) // New kund data
         {
-            var user = await _usermanager.GetUserAsync(User); // Get the logged in User
+            var user = await _userManager.GetUserAsync(User); // Get the logged in User
             if (user == null) return Challenge();
 
-            if (ModelState.IsValid) // Validating the data from the KundViewModel
+            if (!ModelState.IsValid)
             {
-                await _usermanager.RemovePasswordAsync(user); // Removes current password
-                await _usermanager.AddPasswordAsync(user, model.Losenord); // Adds new password
-                var result = await _service.UpdateUserAsync(model.Kund); // Update Kund information in database
-                model.Kund = result; // Sets the updated Kund in the KundViewModel
-
-                return View(model); // returns result
+                return PartialView("_EditDetailsForm", model);
             }
 
-            return View(model); // Returns current state if not valid
+            // Confirm the password is correct before changing details further
+            var checkPassword = await _userManager.CheckPasswordAsync(user, model.Password);
+            if (!checkPassword)
+            {
+                ModelState.AddModelError("Password", "Fel lösenord angett");
+                return PartialView("_EditDetailsForm", model);
+            }
+
+            // Change password if the user has added a new password
+            if (!string.IsNullOrWhiteSpace(model.NewPassword) && !string.IsNullOrWhiteSpace(model.ConfirmPassword))
+            {
+                // Validate the change is confirmed
+                var result = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+                if (result.Succeeded)
+                {
+                    ViewBag.PassWordChanged = "Lösenord har uppdaterats";
+                }
+            }
+
+            var newKundDetails = await _service.UpdateUserAsync(model.Kund);
+            model.Kund = newKundDetails;
+            ViewBag.KundUpdated = "Din data är uppdaterad";
+
+            return PartialView("_EditDetailsForm", model);
         }
 
 
--- TomasosPizzeria/Models/ViewModels/KundEditDetailsViewModel.cs
+++ TomasosPizzeria/Models/ViewModels/KundEditDetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using TomasosPizzeria.Models.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using TomasosPizzeria.Models.Entities;
 
 namespace TomasosPizzeria.Models.ViewModels
 {
@@ -6,8 +7,18 @@ namespace TomasosPizzeria.Models.ViewModels
     {
         public Kund Kund { get; set; }
 
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Du måste ange ditt lösenord")]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Lösenord stämmer inte")]
         public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Text)]
         public string UserName { get; set; }
     }
 }
